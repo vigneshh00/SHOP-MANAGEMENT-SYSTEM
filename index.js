7b2af49e96adcc7da315a3e76da8b0dc04e56a2e
Jazzhamd <jsmhmd184@gmail.com>
2024-04-05 22:24:29 +0530

    changed

--- index.js
+++ index.js
@@ -1,12 +1,30 @@
-const arrow=document.getElementById('arrow');
-arrow.addEventListener('mouseover',()=>{
-    arrow.style.left='260px';
-    if(  arrow.style.left='260px'){
-    arrow.style.transform='scaleX(-1)';}
-    document.getElementById('side-bar').style.width = '250px';
+const sideLines=document.querySelectorAll('.side-line');
+const sideLineContainer=document.getElementById('side-line-container');
+const sideBar=document.getElementById('side-bar');
+function collapseSidebar(){
+    sideLines.forEach( line=>{
+        line.style.marginRight='10px';
+    })
+    sideLineContainer.style.left='0px';
+   sideBar.style.width = '0';
+
+
+}
+ 
+        sideLineContainer.addEventListener('mouseover',()=>{
+           sideLines.forEach(line=>{line.style.marginRight='5px';}) 
+            sideLineContainer.style.left='270px';
+           sideBar.style.width = '250px';
+
+        })
+    
+           
+        
+       
+
+
+
+sideBar.addEventListener('mouseleave',()=>{
+    collapseSidebar();
 });
-document.getElementById('side-bar').addEventListener('mouseleave',()=>{
-    arrow.style.left='0px';
-    arrow.style.transform='scaleX(1)';
-    document.getElementById('side-bar').style.width = '0';
-});
+
