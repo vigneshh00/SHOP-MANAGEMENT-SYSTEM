def71bfe8af0efddc5c6aeedcfdf9e4d2f3e3d30
VIGNESH0073 <vignesh073e@gmail.com>
2024-04-06 21:54:26 +0530

    added validation to signup page

--- signup.js
+++ signup.js
@@ -26,3 +26,57 @@ function myshow2(){
         eye3.style.display="none";
     }
 }
+
+function validateForm(){
+    const forms= document.forms[0];
+
+    let uname = forms.elements[5].value;
+    let email = forms.elements[6].value;
+    let pass = forms.elements[7].value;
+    let cn_pass = forms.elements[8].value;
+
+
+    if (uname.length < 3) {
+        alert("Username must contain atleast 3 characters!");
+        return false;
+    }
+    if(uname.length > 20){
+        alert("Username can only contain a maximum of 20 characters!");
+        return false;
+    }
+    if (!/^[a-zA-Z0-9_]+$/.test(uname)) {
+        alert("Username is invalid!");
+        return false;
+    }
+
+    if(!/\S+@\S+\.\S+/.test(email)){
+        alert("Email is not valid!");
+        return false;
+    }
+    if (pass.length < 8) {
+        alert("Password must contain atleast 8 characters!");
+        return false;
+    }    
+    if (!/[A-Z]/.test(pass)) {
+        alert("Password must contain atleast one uppercase letter!");
+        return false;
+    }
+    if (!/[a-z]/.test(pass)) {
+        alert("Password must contain atleast one lowercase letter!");
+        return false;
+    }
+    if (!/\d/.test(pass)) {
+        alert("Password must contain atleast one digit!");
+        return false;
+    }
+    if (!/[$@!%*?&]/.test(pass)) {
+        alert("Password must contain atleast one special character (!$@!%*?&)");
+        return false;
+    }
+    if(pass != cn_pass){
+        alert("Passwords does not match!");
+        return false;
+    }
+    return true;
+
+}
