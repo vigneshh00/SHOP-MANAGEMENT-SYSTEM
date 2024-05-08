0685c1a1def2f3f1ba37010bc67a956a06a477a7
VIGNESH0073 <vignesh073e@gmail.com>
2024-05-08 15:25:49 +0530

    changed

--- action.php
+++ action.php
@@ -3,10 +3,9 @@ $servername = "localhost";
 $username = "root";
 $password = "";
 $dbname = "shop management system";
-// Create connection
+
 $conn = mysqli_connect($servername, $username, $password,$dbname);
 
-// Check connection
 if (!$conn) {
   die("Connection failed: " . mysqli_connect_error());
 }
